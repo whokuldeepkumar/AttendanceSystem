365bc3abfe4f377a951f77981df59a0c1c5ad2e0
whokuldeepkumar <kuldeepkumarjaipur1999@gmail.com>
2025-12-06 12:58:53 +0530

    Update API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-backend-url.onrender.com/api' // Update after deploying backend
+  apiUrl: 'https://knotend-attendance-backend.onrender.com' // Update after deploying backend
 };
